a4d981e40c97876d21f2aed4d07034f5fac95fb7
Nauman Hafiz <nauman.hafiz@rga.com>
2015-04-21 21:34:02 -0400

    pipe startup to dev null

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -6,4 +6,4 @@ cd /home/ubuntu
 #aws s3 cp s3://code-deploy-test-1/latest/app.yml . --region us-east-1
 
 #run server
-nohup java -jar my-app-1.0-SNAPSHOT.jar server app.yml &
+nohup java -jar my-app-1.0-SNAPSHOT.jar server app.yml >/dev/null 2>&1 &
